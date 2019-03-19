3e9310c6059d72ced07564b003af31648222c925
yojik2901 <yojik2901@gmail.com>
2019-03-19 19:02:00 +0300

    Добавьте файлы проекта.

---
+++ Singleton (одиночка)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton__одиночка_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
